2642485008536c4c47ed6a96351da752c12fb278
Simon Stucki <simon_stucki@hotmail.com>
2020-07-02 18:32:55 +0200

    Refactor base layouting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { Box, Grommet, Heading, Text } from 'grommet';
+import { Grommet } from 'grommet';
 import React from 'react';
 import { RecoilRoot } from 'recoil';
 import Form from './Form';
 import Stats from './Stats';
+import MainContainer from './visual/MainContainer';
+import AppHeader from './visual/AppHeader';
 
 const theme = {
   global: {
@@ -18,36 +20,11 @@ function App() {
   return (
     <RecoilRoot>
       <Grommet theme={theme}>
-        <div className="App">
-          <header className="App-header">
-            <Box
-              pad={{
-                vertical: 'none',
-                horizontal: 'medium',
-              }}
-            >
-              <Box
-                pad={{
-                  vertical: 'none',
-                  horizontal: 'small',
-                }}
-              >
-                <Heading margin="none">Homeoffice Rechner</Heading>
-                <Text>
-                  Der Homeoffice Rechner ermöglicht es Dienstleitungsbetrieben
-                  den Nutzen von Homeoffice abzuschätzen. Der Rechner ist für
-                  KMUs, welche primär Computerbasiert arbeiten, geeignet. Für
-                  den Einsatz bei Grossbetrieben oder handwerklichen Unternehmen
-                  ist er wenig hilfreich.
-                </Text>
-              </Box>
-            </Box>
-            <Form />
-          </header>
-        </div>
-        <div>
+        <MainContainer>
+          <AppHeader />
+          <Form />
           <Stats />
-        </div>
+        </MainContainer>
       </Grommet>
     </RecoilRoot>
   );
---
+++ src/visual/MainContainer.js
@@ -0,0 +1,6 @@
+import { Main } from 'grommet';
+import React from 'react';
+
+const MainContainer = (props) => <Main width={{ max: '1200px' }} {...props} />;
+
+export default MainContainer;

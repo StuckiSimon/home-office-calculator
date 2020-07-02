6a0c10828107409eef8cc58d1fab0edf8a9cf2c5
Simon Stucki <simon_stucki@hotmail.com>
2020-07-02 15:12:19 +0200

    Add dummy visualization for economy

--- src/Stats.js
+++ src/Stats.js
@@ -1,4 +1,13 @@
-import { Accordion, AccordionPanel, Box, Heading } from 'grommet';
+import {
+  Accordion,
+  AccordionPanel,
+  Box,
+  DataTable,
+  Distribution,
+  Heading,
+  Meter,
+  Text,
+} from 'grommet';
 import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { workSpaceState } from './selector';
@@ -33,7 +42,53 @@ function Stats() {
         </Box>
       </Box>
       <Accordion multiple basis="full">
-        <AccordionPanel label="Ökonomie" />
+        <AccordionPanel label="Ökonomie">
+          <Box direction="row">
+            <Distribution
+              values={[
+                { value: 50, color: 'light-3', label: 'Immobilien' },
+                { value: 35, color: 'brand', label: 'Wärmeenergie' },
+                { value: 15, color: 'graph-0', label: 'Parkplätze' },
+              ]}
+            >
+              {(value) => (
+                <Box pad="small" background={value.color} fill>
+                  <Text size="large">
+                    {value.value}% {value.label}
+                  </Text>
+                </Box>
+              )}
+            </Distribution>
+            <StatMeter max={100} current={60} diff={<>40%</>} />
+          </Box>
+          <DataTable
+            columns={[
+              {
+                property: 'name',
+                header: <Text>Name</Text>,
+                primary: true,
+              },
+              {
+                property: 'percent',
+                header: 'Gespart',
+                render: (datum) => (
+                  <Box pad={{ vertical: 'xsmall' }}>
+                    <Meter
+                      values={[{ value: datum.percent }]}
+                      thickness="small"
+                      size="small"
+                    />
+                  </Box>
+                ),
+              },
+            ]}
+            data={[
+              { name: 'Immobilien', percent: 20 },
+              { name: 'Wärmeenergie', percent: 30 },
+              { name: 'Parkplätze', percent: 40 },
+            ]}
+          />
+        </AccordionPanel>
         <AccordionPanel label="Ökologie" />
         <AccordionPanel label="Sozial" />
       </Accordion>

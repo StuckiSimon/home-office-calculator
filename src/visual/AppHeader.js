c6521fff453a594b10179275a0dbbe34189d1057
Simon Stucki <simon_stucki@hotmail.com>
2020-07-03 14:28:30 +0200

    Add logo to Header

--- src/visual/AppHeader.js
+++ src/visual/AppHeader.js
@@ -1,5 +1,6 @@
-import { Box, Heading, Text } from 'grommet';
+import { Box, Button, Header, Heading, Menu, Text } from 'grommet';
 import React from 'react';
+import { ReactComponent as LogoIcon } from './assets/logo.svg';
 
 const AppHeader = () => (
   <Box
@@ -12,13 +13,19 @@ const AppHeader = () => (
         horizontal: 'small',
       }}
     >
-      <Heading
-        margin={{
-          vertical: 'small',
-        }}
-      >
-        Homeoffice Rechner
-      </Heading>
+      <Header>
+        <Box direction="row" pad="small" align="center">
+          <LogoIcon style={{ height: 56, width: 56 }} />
+          <Heading
+            margin={{
+              horizontal: 'small',
+              vertical: 'none',
+            }}
+          >
+            Homeoffice Rechner
+          </Heading>
+        </Box>
+      </Header>
       <Text>
         Der Homeoffice Rechner ermöglicht Schweizer Dienstleistungsbetrieben den
         Nutzen von Homeoffice abzuschätzen. Der Rechner ist für KMUs, welche
